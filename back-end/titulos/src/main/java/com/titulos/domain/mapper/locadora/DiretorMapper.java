7d44f4f118fc3195e823c00ca6f24bf2dc0de6b8
thalysf <thalysfcandido@gmail.com>
2021-11-06 13:20:41 -0300

    Diretor CRUD.

--- back-end/titulos/src/main/java/com/titulos/domain/mapper/locadora/DiretorMapper.java
+++ back-end/titulos/src/main/java/com/titulos/domain/mapper/locadora/DiretorMapper.java
@@ -4,8 +4,12 @@ import com.titulos.domain.dto.locadora.DiretorDto;
 import com.titulos.domain.entity.locadora.Diretor;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface DiretorMapper {
     DiretorDto diretorToDiretorDto(Diretor diretor);
     Diretor diretorDtoToDiretor(DiretorDto diretorDto);
+
+    List<DiretorDto> listDiretorToListDiretorDto(List<Diretor> diretores);
 }
--- back-end/titulos/src/main/java/com/titulos/service/locadora/DiretorService.java
+++ back-end/titulos/src/main/java/com/titulos/service/locadora/DiretorService.java
@@ -1,7 +1,88 @@
 package com.titulos.service.locadora;
 
+import com.titulos.domain.dto.locadora.DiretorDto;
+import com.titulos.domain.mapper.locadora.DiretorMapper;
+import com.titulos.repository.locadora.DiretorRepository;
+import com.titulos.util.commons.Validate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
 
 @Service
 public class DiretorService {
+    @Autowired
+    private DiretorRepository diretorRepository;
+
+    @Autowired
+    private DiretorMapper diretorMapper;
+
+    @Autowired
+    private Validate validate;
+
+    public DiretorDto cadastrarDiretor(DiretorDto diretorDto)
+    {
+        if(!validate.isNullOrEmpty(String.valueOf(diretorDto.getIdDiretor())))
+        {
+            throw new IllegalArgumentException("ID do diretor não deve ser enviado na inserção!");
+        }
+        isValidDiretor(diretorDto);
+
+        var diretorEntity = diretorMapper.diretorDtoToDiretor(diretorDto);
+        return diretorMapper.diretorToDiretorDto(diretorRepository.save(diretorEntity));
+    }
+
+    public DiretorDto atualizarDiretor(DiretorDto diretorDto) {
+        if(validate.isNullOrEmpty(String.valueOf(diretorDto.getIdDiretor())))
+        {
+            throw new IllegalArgumentException("ID do diretor não pode estar vazio ou nulo");
+        }
+        isValidDiretor(diretorDto);
+        listarDiretor(String.valueOf(diretorDto.getIdDiretor()));
+        var diretorEntity = diretorMapper.diretorDtoToDiretor(diretorDto);
+        return diretorMapper.diretorToDiretorDto(diretorRepository.save(diretorEntity));
+    }
+
+    public void deletarDiretor(String id_diretor) {
+        if(validate.isNullOrEmpty(id_diretor))
+        {
+            throw new IllegalArgumentException("ID do diretor não pode estar vazio ou nulo");
+        }
+
+        var idDiretor = Long.parseLong(id_diretor);
+        try {
+            diretorRepository.deleteById(idDiretor);
+        }
+        catch (EmptyResultDataAccessException e)
+        {
+            throw new EntityNotFoundException("Diretor de id " + id_diretor + " não existe! Portanto não pode ser deletado!");
+        }
+    }
+
+    public DiretorDto listarDiretor(String id_diretor) {
+        var idDiretor = Long.parseLong(id_diretor);
+        DiretorDto diretorDto = new DiretorDto();
+        try{
+            diretorDto = diretorMapper.diretorToDiretorDto(diretorRepository.getById(idDiretor));
+        }
+        catch (EntityNotFoundException e)
+        {
+            throw new EntityNotFoundException("Diretor de id " + id_diretor + " não encontrado! Operação não pode ser efetuada!");
+        }
+        return diretorDto;
+    }
+
+    public List<DiretorDto> listarDiretores() {
+        return diretorMapper.listDiretorToListDiretorDto(diretorRepository.findAll());
+    }
+
+    private void isValidDiretor(DiretorDto diretorDto)
+    {
+        if(validate.isNullOrEmpty(diretorDto.getNome()))
+        {
+            throw new IllegalArgumentException("Nome do diretor não pode ser nulo ou vazio");
+        }
+    }
 }
